25710609a47a65098bf8d698d1e5cb086de014a3
itachi-108 <dipanshubeda@gmail.com>
2026-09-04 22:26:41 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -1,6 +1,6 @@
 // songs.js
 const chapterSongs = {
-    1: "song1.mp3", // Replace with your exact first song file name
-    2: "song2.mp3", // Replace with your exact second song file name
-    3: "song3.mp3"  // Replace with your exact third song file name
+    1: "https://files.catbox.moe/8otrlv.mp3",
+    2: "https://files.catbox.moe/ot49l6.mp3",
+    3: "https://files.catbox.moe/kh4k38.mp3"
 };
