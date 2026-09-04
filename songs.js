a5c00fc9968ae01ce4f7465bac5f8fc28e45d2a5
itachi-108 <dipanshubeda@gmail.com>
2026-09-04 23:13:00 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -1,6 +1,6 @@
 // songs.js
 const chapterSongs = {
-    1: "https://files.catbox.moe/8otrlv.mp3",
-    2: "https://files.catbox.moe/ot49l6.mp3",
+    1: "https://files.catbox.moe/53ki9f.mp3", 
+    2: "https://files.catbox.moe/ot49l6.mp3",// Bulleya
     3: "https://files.catbox.moe/kh4k38.mp3"
 };
