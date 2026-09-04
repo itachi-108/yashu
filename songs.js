d86e5c18a88734572057e4d50901679cd92def74
itachi-108 <dipanshubeda@gmail.com>
2026-09-04 20:29:07 +0530

    Create songs.js

---
+++ songs.js
@@ -0,0 +1,6 @@
+// Dedicated configuration file for your chapter songs
+const chapterSongs = {
+    1: "bulleya_instrumental.mp3",
+    2: "tu_chahiye_instrumental.mp3",
+    3: "chaar_kadam_instrumental.mp3"
+};
